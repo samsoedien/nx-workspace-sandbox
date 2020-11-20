0472df5d647d45f9afe9593a4f49118e32f62608
Samsoedien <contact@samsoedien.com>
2020-11-20 16:34:19 +0100

    feat: created react and angular apps

---
+++ apps/angular-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-workspace-sandbox-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-app';
+}
